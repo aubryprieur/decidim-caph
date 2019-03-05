1110a2c9d841cacfbed3a382bc1641c2969b5558
aubry prieur <aubry.prieur@gmail.com>
2019-03-05 10:34:12 +0100

    ajout config carrierwave, buccketer

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -5,8 +5,8 @@ Decidim.configure do |config|
   config.mailer_sender = "change-me@domain.org"
 
   # Change these lines to set your preferred locales
-  config.default_locale = :en
-  config.available_locales = [:en, :ca, :es]
+  config.default_locale = :fr
+  config.available_locales = [:fr, :en]
 
   # Geocoder configuration
   # config.geocoder = {
